3336d57b08d80b91859bb2ec4c0f09d2a22186d1
Gabor Borbely <gaebor@gmail.com>
2020-10-28 22:57:26 +0100

    removed one file

--- checksums.sh
+++ checksums.sh
@@ -1,4 +1,4 @@
-md5sum archive.sh run.sh run.cmd \
+md5sum archive.sh run.sh \
     `find hazijavitorendszer/ -maxdepth 1 -type f` \
     `find hazijavitorendszer/HW/ -maxdepth 1 -type f | grep -v '.answer' | grep -v '\.*.tsv'` | sort | (
 if [ "$1" = "-l" -o "$1" = "--list" -o "$1" = "--long" ]
