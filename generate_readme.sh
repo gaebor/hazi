e0d37d638c1c386eb6cf7c783ea69fd4383b058a
Gábor Borbély <gaebor@gmail.com>
2021-12-28 11:24:56 +0100

    update generator to produce hashes in readme

--- generate_readme.sh
+++ generate_readme.sh
@@ -1,9 +1,21 @@
-cat > README.md << EOF
-# hazi
+echo "# hazi
 Automated Email Exercise Evaluator
+"
 
-## In action:
+echo -n "## MD5
+This version's MD5 hash is:
+
+    "
+bash checksums.sh | cut -f1 -d' '
+echo "
+In detail:
+"
+bash checksums.sh -l | sed -e 's/^/    /'
+
+echo "
+Use \`checksums.sh\` to check MD5 hash.
+"
+echo "## In action:
 
 * http://wiki.math.bme.hu/view/HazifeladatEllenorzoTeacher
-* http://math.bme.hu/u/hazi
-EOF
+* http://math.bme.hu/u/hazi"
